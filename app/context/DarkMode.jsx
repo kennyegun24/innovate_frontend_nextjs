fb8d091f254facab1851198a613c4a4cea3c905d
Kenny Egun <kennyegun240@gmail.com>
2023-09-15 17:00:37 +0100

    Modify

--- app/context/DarkMode.jsx
+++ app/context/DarkMode.jsx
@@ -12,7 +12,7 @@ export const ThemeProvider = ({ children }) => {
 
   return (
     <ThemeContext.Provider value={{ toggle, mode }}>
-      <div className={`theme ${mode}`}>{children}</div>
+      <div className={`theme ${mode} bigContainer`}>{children}</div>
     </ThemeContext.Provider>
   );
 };
