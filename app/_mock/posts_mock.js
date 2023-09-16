6e84d8ca15a7cf6b5b1eb22efaacdaed773b127f
Kenny Egun <kennyegun240@gmail.com>
2023-09-16 20:46:54 +0100

    Modified

--- app/_mock/posts_mock.js
+++ app/_mock/posts_mock.js
@@ -1,37 +1,58 @@
+import rooney from "public/rooney.jpg";
+import rashford from "public/rashford.jpg";
+import messi from "public/messi.jpg";
+import ronaldo from "public/ronaldo.jpg";
+import hojlund from "public/hojlund.jpg";
+
 export const posts = [
   {
     creator_name: "Killian",
+    id: 4,
     text: "the few owner invite like invite like the few owner in the same way itself invite invite.. the whole",
     author_id: 1,
     likes_count: 2330931400,
     comments_count: 32514679110,
+    creator_image: rooney,
+    image: hojlund,
   },
   {
     creator_name: "Jonah",
-    text: "the most water celebrate because of celebrate because of the most water such as mine celebrate celeb",
+    id: 2,
+    text: "the most water celebrate because of celebrate because of \n the most water such as mine celebrate celeb",
     author_id: 2,
     likes_count: 66543693260,
     comments_count: 46625487324,
+    creator_image: rashford,
+    image: rooney,
   },
   {
     creator_name: "Lindon",
+    id: 5,
     text: "the entire opinion pour aside from pour aside from the entire opinion on top of that whoever pour po",
     author_id: 3,
     likes_count: 78688108883,
     comments_count: 2748118853,
+    creator_image: messi,
+    image: null,
   },
   {
     creator_name: "Tanney",
-    text: "various justification accept along with accept along with various justification while who accept acc",
+    id: 1,
+    text: "various justification accept along with accept along with various justification \n while who accept acc",
     author_id: 4,
     likes_count: 39686946807,
     comments_count: 98917748491,
+    creator_image: ronaldo,
+    image: rashford,
   },
   {
     creator_name: "Geordie",
-    text: "the previous unit jump with respect to jump with respect to the previous unit undeniably it jump jum",
+    id: 3,
+    text: "the previous unit jump with respect to jump with respect to the previous unit \nundeniably it jump jum",
     author_id: 5,
     likes_count: 39519305390,
     comments_count: 29211288816,
+    creator_image: hojlund,
+    image: null,
   },
 ];
