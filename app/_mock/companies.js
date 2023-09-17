10a354ae2468932ae9d7cf244dc076ab2f6819d1
Kenny Egun <kennyegun240@gmail.com>
2023-09-17 11:12:57 +0100

    Add extra data

--- app/_mock/companies.js
+++ app/_mock/companies.js
@@ -7,30 +7,35 @@ import hojlund from "public/rooney.jpg";
 export const companies = [
   {
     company_name: "Chevrolet",
+    isFollowed: false,
     company_id: 1,
     image: ronaldo,
     field: "Systems Engineer",
   },
   {
     company_name: "Volkswagen",
+    isFollowed: false,
     company_id: 2,
     image: hojlund,
     field: "Piano Tuner",
   },
   {
     company_name: "Cadillac",
+    isFollowed: false,
     company_id: 3,
     image: rooney,
     field: "Veterinary Surgeon",
   },
   {
     company_name: "Suzuki",
+    isFollowed: false,
     company_id: 4,
     image: messi,
     field: "Cafe Staff",
   },
   {
     company_name: "BMW",
+    isFollowed: false,
     company_id: 5,
     image: rashford,
     field: "Nursery Assistant",
--- app/_mock/posts_mock.js
+++ app/_mock/posts_mock.js
@@ -7,6 +7,7 @@ import hojlund from "public/hojlund.jpg";
 export const posts = [
   {
     creator_name: "Killian",
+    isLiked: false,
     id: 4,
     text: "the few owner invite like invite like the few owner in the same way itself invite invite.. the whole",
     author_id: 1,
@@ -17,6 +18,7 @@ export const posts = [
   },
   {
     creator_name: "Jonah",
+    isLiked: false,
     id: 2,
     text: "the most water celebrate because of celebrate because of \n the most water such as mine celebrate celeb",
     author_id: 2,
@@ -27,6 +29,7 @@ export const posts = [
   },
   {
     creator_name: "Lindon",
+    isLiked: false,
     id: 5,
     text: "the entire opinion pour aside from pour aside from the entire opinion on top of that whoever pour po",
     author_id: 3,
@@ -37,6 +40,7 @@ export const posts = [
   },
   {
     creator_name: "Tanney",
+    isLiked: false,
     id: 1,
     text: "various justification accept along with accept along with various justification \n while who accept acc",
     author_id: 4,
@@ -47,6 +51,7 @@ export const posts = [
   },
   {
     creator_name: "Geordie",
+    isLiked: false,
     id: 3,
     text: "the previous unit jump with respect to jump with respect to the previous unit \nundeniably it jump jum",
     author_id: 5,
