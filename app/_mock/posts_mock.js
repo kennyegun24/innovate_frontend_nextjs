09c55c48feed641967c40886dada6137ea60fea3
Kenny Egun <kennyegun240@gmail.com>
2023-09-18 00:37:49 +0100

    comments mock

--- app/_mock/posts_mock.js
+++ app/_mock/posts_mock.js
@@ -8,7 +8,7 @@ export const posts = [
   {
     creator_name: "Killian",
     isLiked: false,
-    id: 4,
+    id: 0,
     text: "the few owner invite like invite like the few owner in the same way itself invite invite.. the whole",
     author_id: 1,
     likes_count: 2330931400,
@@ -19,7 +19,7 @@ export const posts = [
   {
     creator_name: "Jonah",
     isLiked: false,
-    id: 2,
+    id: 1,
     text: "the most water celebrate because of celebrate because of \n the most water such as mine celebrate celeb",
     author_id: 2,
     likes_count: 66543693260,
@@ -30,7 +30,7 @@ export const posts = [
   {
     creator_name: "Lindon",
     isLiked: false,
-    id: 5,
+    id: 2,
     text: "the entire opinion pour aside from pour aside from the entire opinion on top of that whoever pour po",
     author_id: 3,
     likes_count: 78688108883,
@@ -41,7 +41,7 @@ export const posts = [
   {
     creator_name: "Tanney",
     isLiked: false,
-    id: 1,
+    id: 3,
     text: "various justification accept along with accept along with various justification \n while who accept acc",
     author_id: 4,
     likes_count: 39686946807,
@@ -52,7 +52,7 @@ export const posts = [
   {
     creator_name: "Geordie",
     isLiked: false,
-    id: 3,
+    id: 4,
     text: "the previous unit jump with respect to jump with respect to the previous unit \nundeniably it jump jum",
     author_id: 5,
     likes_count: 39519305390,
