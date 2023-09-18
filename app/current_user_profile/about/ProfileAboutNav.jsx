4811d4dc987ccaee4aa616380879aa76c097ac2a
Kenny Egun <kennyegun240@gmail.com>
2023-09-18 22:17:11 +0100

    Activate tabs to switch between profile overview comps

--- app/current_user_profile/about/ProfileAboutNav.jsx
+++ app/current_user_profile/about/ProfileAboutNav.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { BiBriefcase } from "react-icons/bi";
 import { BsInfo } from "react-icons/bs";
 import { FaCheckCircle, FaGraduationCap } from "react-icons/fa";
 import Overview from "./Overview";
 
 const ProfileAboutNav = () => {
+  const [tab, setTab] = useState(1);
   return (
     <section className="flex gap2rem">
       <div
@@ -16,33 +18,35 @@ const ProfileAboutNav = () => {
         }}
       >
         <p
-          className="font14 flex align_center gap05rem padding1rem"
+          className="font14 pointer flex align_center gap05rem padding1rem"
           style={{ background: "#111", borderRadius: "6px" }}
+          onClick={() => setTab(1)}
         >
           <FaCheckCircle /> Overview
         </p>
         <p
-          className="font14 flex align_center gap05rem padding1rem"
+          className="font14 pointer flex align_center gap05rem padding1rem"
           style={{ background: "#111", borderRadius: "6px" }}
+          onClick={() => setTab(2)}
         >
           <BsInfo /> Personal Info
         </p>
         <p
-          className="font14 flex align_center gap05rem padding1rem"
+          className="font14 pointer flex align_center gap05rem padding1rem"
           style={{ background: "#111", borderRadius: "6px" }}
+          onClick={() => setTab(3)}
         >
           <FaGraduationCap /> Education
         </p>
         <p
-          className="font14 flex align_center gap05rem padding1rem"
+          className="font14 pointer flex align_center gap05rem padding1rem"
           style={{ background: "#111", borderRadius: "6px" }}
+          onClick={() => setTab(4)}
         >
           <BiBriefcase /> Experience
         </p>
       </div>
-      <div style={{ width: "75%" }}>
-        <Overview />
-      </div>
+      <div style={{ width: "75%" }}>{tab === 1 && <Overview />}</div>
     </section>
   );
 };
