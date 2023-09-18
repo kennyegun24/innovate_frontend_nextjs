30b68212eef0af571040b91f8a380975e4f5ddec
Kenny Egun <kennyegun240@gmail.com>
2023-09-18 20:49:38 +0100

    Overview component

---
+++ app/current_user_profile/about/Overview.jsx
@@ -0,0 +1,8 @@
+import ProfileOverview from "@/app/components/profile/profileAbout/ProfileOverview";
+import React from "react";
+
+const Overview = () => {
+  return <ProfileOverview />;
+};
+
+export default Overview;
