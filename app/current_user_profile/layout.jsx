61774f46422c8c8666e5799f27687e4d2cd7df7a
Kenny Egun <kennyegun240@gmail.com>
2023-09-18 17:52:49 +0100

    currentuser profile page

---
+++ app/current_user_profile/layout.jsx
@@ -0,0 +1,27 @@
+"use client";
+import React, { useContext } from "react";
+import ProfileNav from "@/app/components/profile/profileNav/ProfileNav";
+import Header from "./Header";
+import styles from "./page.module.css";
+import { ThemeContext } from "../context/DarkMode";
+
+const Layout = ({ children }) => {
+  const { mode } = useContext(ThemeContext);
+  return (
+    <div className={styles.layoutContainer}>
+      <div
+        className={`theme ${
+          mode === "light" ? "light_background" : "dark_2_background"
+        } ${styles.layoutProfileNavDiv}`}
+      >
+        <ProfileNav />
+      </div>
+      <div className={styles.layoutProfileChildren}>
+        <Header mode={mode} />
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
