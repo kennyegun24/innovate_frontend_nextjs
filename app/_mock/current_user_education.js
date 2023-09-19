9db48cce381d6b8a89ee231de0fa4c9d13bf45b5
Kenny Egun <kennyegun240@gmail.com>
2023-09-19 21:08:32 +0100

    current user education mock file

---
+++ app/_mock/current_user_education.js
@@ -0,0 +1,38 @@
+import rooney from "public/rooney.jpg";
+import hojlund from "public/hojlund.jpg";
+import messi from "public/messi.jpg";
+import ronaldo from "public/ronaldo.jpg";
+export const currentUserEducation = [
+  {
+    school_name: "Harvard",
+    degree: "Masters",
+    year: "2020",
+    course: "Computer Engineering",
+    desc: "Learnt Artificial intelligence and created several AI model to help solve algorithmic problems",
+    img: rooney,
+  },
+  {
+    school_name: "Oxford University",
+    degree: "Phd",
+    year: "2021",
+    course: "Computer Science",
+    desc: "Learnt Artificial intelligence and created several AI model to help solve algorithmic problems",
+    img: hojlund,
+  },
+  {
+    school_name: "MIT",
+    degree: "Bsc",
+    year: "2016",
+    course: "Computer Science",
+    desc: "Learnt Artificial intelligence and created several AI model to help solve algorithmic problems",
+    img: messi,
+  },
+  {
+    school_name: "Cambridge University",
+    degree: "Diploma",
+    year: "2020",
+    course: "Computer Engineering",
+    desc: "Learnt Artificial intelligence and created several AI model to help solve algorithmic problems",
+    img: ronaldo,
+  },
+];
