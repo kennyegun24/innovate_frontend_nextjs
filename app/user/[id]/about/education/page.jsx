c6092555d5eeb327c1c7286befd36a5241a0b56f
Kenny Elias <kennyegun240@gmail.com>
2023-09-20 21:27:17 +0100

    other user profile completition

--- app/user/[id]/about/education/page.jsx
+++ app/user/[id]/about/education/page.jsx
@@ -1,9 +1,13 @@
+"use client";
 import { currentUserEducation } from "@/app/_mock/current_user_education";
 import ProfileEducation from "@/app/components/profile/profileAbout/ProfileEducation";
 import React from "react";
+import { usePathname } from "next/navigation";
+import { otherUserDetails } from "@/app/_mock/user_details";
 
 const CurrentUserEducation = () => {
-  return <ProfileEducation data={currentUserEducation} />;
+  const pathId = usePathname().split("/")[2];
+  return <ProfileEducation data={otherUserDetails[pathId].education} />;
 };
 
 export default CurrentUserEducation;
--- app/user/[id]/about/experience/page.jsx
+++ app/user/[id]/about/experience/page.jsx
@@ -1,9 +1,13 @@
+"use client";
 import { currentUserExperience } from "@/app/_mock/current_user_experience";
 import ProfileExperience from "@/app/components/profile/profileAbout/ProfileExperience";
 import React from "react";
 
+import { otherUserDetails } from "@/app/_mock/user_details";
+import { usePathname } from "next/navigation";
 const CurrentUserExperience = () => {
-  return <ProfileExperience data={currentUserExperience} />;
+  const pathId = usePathname().split("/")[2];
+  return <ProfileExperience data={otherUserDetails[pathId].experience} />;
 };
 
 export default CurrentUserExperience;
