c2732489351e5f84a16fca5876554f6c3397cd05
Kenny Elias <kennyegun240@gmail.com>
2023-09-28 20:38:44 +0100

    Add back button to profile

--- app/current_user_profile/layout.jsx
+++ app/current_user_profile/layout.jsx
@@ -2,14 +2,27 @@ import React from "react";
 import ProfileNav from "@/app/components/profile/profileNav/ProfileNav";
 import Header from "./Header";
 import styles from "./page.module.css";
+import Back from "../components/Back";
 
 const Layout = ({ children }) => {
   return (
     <div className={styles.layoutContainer}>
       <div className={`theme background2 ${styles.layoutProfileNavDiv}`}>
         <ProfileNav />
+        <div
+          style={{
+            position: "absolute",
+            right: 0,
+            top: 0,
+          }}
+        >
+          <Back />
+        </div>
       </div>
       <div className={styles.layoutProfileChildren}>
+        <div className={styles.backDiv}>
+          <Back />
+        </div>
         <Header />
         <div className={styles.layoutProfileChildrenDiv}>{children}</div>
       </div>
