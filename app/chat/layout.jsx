53d1718e4705b36f201ac7127dbbb10ab1a0fe8d
Kenny Elias <kennyegun240@gmail.com>
2023-09-30 14:07:43 +0100

    All chat component and create basic chat page structure

--- app/chat/layout.jsx
+++ app/chat/layout.jsx
@@ -4,11 +4,17 @@ import styles from "./layout.module.css";
 
 const layout = ({ children }) => {
   return (
-    <div className="flex ">
-      <div className={styles.layoutAllChatsDiv}>
-        <AllChats />
+    <div
+      className={`flex align_center justify_center ${styles.layoutContainer}`}
+    >
+      <div
+        className={`flex align_Center border background2 theme ${styles.subLayoutContainer}`}
+      >
+        <div className={`scroll_y ${styles.layoutAllChatsDiv}`}>
+          <AllChats />
+        </div>
+        <div className={styles.layoutChildren}>{children}</div>
       </div>
-      <div className={styles.layoutChildren}>{children}</div>
     </div>
   );
 };
--- app/components/chat/AllChats.jsx
+++ app/components/chat/AllChats.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { _mock_all_chats } from "@/app/_mock/all_message";
+import Image from "next/image";
+import { convertTime } from "@/app/helpers/general";
 
 const AllChats = () => {
-  return <div>AllChats</div>;
+  return (
+    <div className="flex column gap05rem">
+      {_mock_all_chats.map((user, index) => (
+        <>
+          <div key={index} className="flex gap05rem">
+            <Image
+              src={user.image}
+              alt=""
+              width={50}
+              height={50}
+              className="roundedImage object-cover"
+            />
+            <div
+              className="flex column gap02rem"
+              style={{
+                flex: 1,
+              }}
+            >
+              <p className="font14">{user.display_name}</p>
+              <p className="font12">
+                {user.last_message.length >= 40
+                  ? user.last_message.slice(0, 40) + "..."
+                  : user.last_message}
+              </p>
+              <p className="font10 opacity05">{convertTime(user.time)}</p>
+            </div>
+          </div>
+          <hr className="hr" />
+        </>
+      ))}
+    </div>
+  );
 };
 
 export default AllChats;
