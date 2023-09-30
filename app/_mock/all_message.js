096425c7744814c7404409569eb64775848d4d00
Kenny Elias <kennyegun240@gmail.com>
2023-09-30 09:42:27 +0100

    chat mock data

---
+++ app/_mock/all_message.js
@@ -0,0 +1,62 @@
+export const _mock_all_chats = [
+  {
+    display_name: "Kalle",
+    last_message:
+      "the entire Court let by means of let by means of the entire Court on the contrary yourself let let.. plenty of purchase drive out drive out plenty of purchase especially we drive drive..",
+    time: "10/9/1993",
+  },
+  {
+    display_name: "Humphrey",
+    last_message:
+      "the current alliance control close to control close to the current alliance whether... or whoever control control.. the following kingdom lend in addition to lend in addition to the following kingdom in summary who lend lend..",
+    time: "2/1/1957",
+  },
+  {
+    display_name: "Riccardo",
+    last_message:
+      "the previous fee eat along with eat along with the previous fee in fact myself eat eat.. this show invest on top of invest on top of this show particularly her invest invest..",
+    time: "30/6/1964",
+  },
+  {
+    display_name: "Guillaume",
+    last_message:
+      "the numerous deck add in front of add in front of the numerous deck by the way hers add add.. the last walk continue out of continue out of the last walk on the contrary that continue continue..",
+    time: "13/3/1965",
+  },
+  {
+    display_name: "Forbes",
+    last_message:
+      "the quaternary metal remember because of remember because of the quaternary metal in brief my remember remember.. the different police marry aside from marry aside from the different police alternatively whoever marry marry..",
+    time: "31/1/2003",
+  },
+  {
+    display_name: "Hy",
+    last_message:
+      "the penultimate bronze regret against regret against the penultimate bronze on the other hand its regret regret.. any print point on account of point on account of any print notably themselves point point..",
+    time: "3/3/1950",
+  },
+  {
+    display_name: "Leopold",
+    last_message:
+      "the last operator promise like promise like the last operator therefore whoever promise promise.. few boot prepare in case of prepare in case of few boot provided that whoever prepare prepare..",
+    time: "20/5/2000",
+  },
+  {
+    display_name: "Ogden",
+    last_message:
+      "her bank burn in lieu of burn in lieu of her bank provided that its burn burn.. the various directory listen in case of listen in case of the various directory on the whole hers listen listen..",
+    time: "17/9/1980",
+  },
+  {
+    display_name: "Alonzo",
+    last_message:
+      "the ultimate adjustment represent outside represent outside the ultimate adjustment alternatively what represent represent.. whose meal settle out of settle out of whose meal still she settle settle..",
+    time: "4/10/1995",
+  },
+  {
+    display_name: "Gav",
+    last_message:
+      "the whole dealer establish as far as establish as far as the whole dealer to give an example we establish establish.. the previous invitation manage close to manage close to the previous invitation provided that whomever manage manage..",
+    time: "15/8/1978",
+  },
+];
