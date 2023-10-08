b92970f48444fdff74c8405f92f624b124cd7bd5
Kenny Elias <kennyegun240@gmail.com>
2023-10-08 19:30:50 +0100

    Modify seo

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    metadataBase: "https://innovate-frontend-nextjs.vercel.app/",
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
