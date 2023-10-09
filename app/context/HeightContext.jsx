54c7573bbbc4c6acfa04d7a8032c575e88a8e502
Kenny Elias <kennyegun240@gmail.com>
2023-10-09 19:57:27 +0100

    Modify to handle scroll of landing page for special effect on nav comp

--- app/context/HeightContext.jsx
+++ app/context/HeightContext.jsx
@@ -5,28 +5,47 @@ export const HeightContext = createContext();
 
 export const HeightProvider = ({ children }) => {
   const [screenWidth, setScreenWidth] = useState(0);
+  const [scrollY, setScrollY] = useState(100);
+  const [element, setElement] = useState(null);
 
   useEffect(() => {
-    // Function to update screenWidth state
     const updateScreenWidth = () => {
       setScreenWidth(window.innerWidth);
     };
 
-    // Initial screen width
     updateScreenWidth();
-    console.log("fskdjs");
 
-    // Attach event listener to window resize event
     window.addEventListener("resize", updateScreenWidth);
 
-    // Cleanup by removing the event listener when the component unmounts
     return () => {
       window.removeEventListener("resize", updateScreenWidth);
     };
   }, []);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (element !== null) {
+        const scrollTotal = document.querySelector(".landingPage")?.scrollTop;
+
+        setScrollY(scrollTotal);
+      } else {
+        setScrollY(0);
+      }
+    };
+    handleScroll();
+
+    document
+      .querySelector(".landingPage")
+      ?.addEventListener("scroll", handleScroll);
+    return () => {
+      document
+        .querySelector(".landingPage")
+        ?.removeEventListener("scroll", handleScroll);
+    };
+  }, [element]);
+
   return (
-    <HeightContext.Provider value={{ screenWidth }}>
+    <HeightContext.Provider value={{ screenWidth, scrollY, setElement }}>
       <div>{children}</div>
     </HeightContext.Provider>
   );
