25124c6b8423fe33205933f39fea0d702d7696f0
Kenny Elias <kennyegun240@gmail.com>
2023-10-12 01:10:05 +0100

    fix

--- app/layout.js
+++ app/layout.js
@@ -31,18 +31,12 @@ export const metadata = {
   },
 };
 
-const no_nav_links = [
-  "/",
-  "/about",
-  "/about_creator",
-  "/login",
-  "/register",
-  "",
-];
+const no_nav_links = ["/", "/about", "/about_creator", "/login", "/register"];
 
 export default function RootLayout({ children }) {
   const headersList = headers();
   const pathname = headersList.get("x-invoke-path") || "";
+  console.log(pathname);
   return (
     <html lang="en">
       <body className={` ${inter.className}`}>
