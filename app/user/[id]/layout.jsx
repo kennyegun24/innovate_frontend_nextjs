980e7d297c2c6695ffa5acfbffc8abefcd6fd367
Kenny Elias <kennyegun240@gmail.com>
2023-10-16 01:16:57 +0100

    Improve SEO for user details page

--- app/user/[id]/layout.jsx
+++ app/user/[id]/layout.jsx
@@ -1,27 +1,32 @@
-"use client";
 import React from "react";
 import ProfileNav from "@/app/components/profile/profileNav/ProfileNav";
 import Header from "./Header";
 import styles from "./page.module.css";
 import Back from "@/app/components/Back";
+import { users } from "@/app/_mock/users";
 
+export const generateMetadata = ({ params }) => {
+  const data = users[params.id];
+  return {
+    title: `${data.name} profile`,
+    description: `profession: ${data.profession} ~ Name: ${data.name}`,
+    twitter: {
+      title: `${data.name} profile`,
+      description: `profession: ${data.profession} ~ Name: ${data.name}`,
+      text: `I am ${data.name}`,
+    },
+    openGraph: {
+      title: `${data.name} profile`,
+      description: `profession: ${data.profession} | Name: ${data.name}`,
+      image: data.image,
+    },
+  };
+};
 const Layout = ({ children }) => {
   return (
     <div className={styles.layoutContainer}>
       <Header />
-      <div
-        style={{
-          position: "absolute",
-          left: 0,
-          top: 0,
-        }}
-        className={styles.backDiv1}
-      >
-        <Back />
-      </div>
-      <div className={styles.backDiv2}>
-        <Back />
-      </div>
+
       <div className={styles.layoutProfileChildren}>{children}</div>
     </div>
   );
