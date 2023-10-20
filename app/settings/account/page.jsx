7d28fa08c9b3c9cd94882fa8bd26bce2678709b4
Kenny Elias <kennyegun240@gmail.com>
2023-10-21 00:17:42 +0100

    Create logout func seprate accounts settings comp

--- app/settings/account/page.jsx
+++ app/settings/account/page.jsx
@@ -1,61 +1,14 @@
-import Link from "next/link";
 import React from "react";
-import { BsLockFill } from "react-icons/bs";
-import { FaLock, FaTrash, FaUser } from "react-icons/fa";
-import { TiUserDelete } from "react-icons/ti";
-import styles from "./styles.module.css";
-import { DeleteFilled } from "@ant-design/icons";
+import AccountSettings from "./AccountSettings";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-const links = [
-  {
-    name: "Account Information",
-    text: "Check your account information",
-    link: "information",
-    icon: FaUser,
-  },
-  {
-    name: "Change Password",
-    text: "Secure your account by changing your account password anytime you want",
-    link: "password_change",
-    icon: FaLock,
-  },
-  {
-    name: "Deactivate Account",
-    text: "Deavtivate account... WARNING!!! ACTION CANNOT BE REVOKED IF DONE!!",
-    link: "deactivate",
-    icon: FaTrash,
-  },
-];
-const Account = () => {
-  return (
-    <div>
-      <div className="padding1rem flex column gap15rem">
-        <h4 className={styles.header}>Your Account</h4>
-        <p className={`opacity07 ${styles.headerText}`}>
-          Change password, check account information, deactivate account
-          (WARNING! CANNOT BE REVOKED, AS ALL USER DETAILS WILL CEASE TO EXIST
-          IN OUR DATABASE)
-        </p>
-      </div>
-      {links.map((link, index) => (
-        <Link
-          key={index}
-          className={`linkBackgroundNone text_decoration_none text_color`}
-          href={`/settings/account/${link.link}`}
-        >
-          <div
-            className={`flex gap15rem align_center ${styles.mappedSettings}`}
-          >
-            <link.icon className={`${styles.icon}`} />
-            <div className={styles.width85}>
-              <p className={`fontW600 ${styles.linkName}`}>{link.name}</p>
-              <p className={`opacity07 ${styles.linkText}`}>{link.text}</p>
-            </div>
-          </div>
-        </Link>
-      ))}
-    </div>
-  );
+const Account = async () => {
+  const session = await getServerSession();
+  if (!session) {
+    redirect("/login");
+  }
+  return <AccountSettings />;
 };
 
 export default Account;
