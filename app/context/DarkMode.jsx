458f56d55d70a7477df65d33329cc38a720eecd1
Kenny Elias <kennyegun240@gmail.com>
2023-10-21 16:22:55 +0100

    Add persist storage, but comment it out due to white screen flashes on refresh

--- app/context/DarkMode.jsx
+++ app/context/DarkMode.jsx
@@ -2,6 +2,7 @@
 import { createContext, useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import { store, wrapper } from "../redux/store";
+// import { PersistGate } from "redux-persist/integration/react";
 
 export const ThemeContext = createContext();
 
@@ -19,9 +20,11 @@ export const ThemeProvider = ({ children }) => {
   return (
     <ThemeContext.Provider value={{ toggle, mode }}>
       <Provider store={store}>
+        {/* <PersistGate loading={null} persistor={persistor}> */}
         <div className={`theme background bigContainer relative text_color`}>
           {children}
         </div>
+        {/* </PersistGate> */}
       </Provider>
     </ThemeContext.Provider>
   );
