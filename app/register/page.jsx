3d07a67c497b2aa967938af1d645ac0e0e37a871
Kenny Elias <kennyegun240@gmail.com>
2023-10-21 22:16:10 +0100

    Separate reg form from page

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,95 +1,14 @@
-"use client";
-import React, { useState } from "react";
-import styles from "./register.module.css";
-import { registerAuthentication } from "@/app/utils/api_requests/auth";
-import { useDispatch, useSelector } from "react-redux";
-import Image from "next/image";
-import imagePicker from "public/image_picker32.png";
-import { loginFailure } from "../redux/user_auth/userReducer";
+import React from "react";
+import Form from "../login/Form";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-const Registration = () => {
-  const [userData, setUserData] = useState(null);
-  const [file, setFile] = useState(null);
-  const dispatch = useDispatch();
-  const { detailsError } = useSelector((state) => state.user);
-  const handleInput = (e) => {
-    setUserData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
-
-  const onSub = async (e) => {
-    e.preventDefault();
-    const id = crypto.randomUUID();
-    try {
-      await registerAuthentication(
-        {
-          data: userData,
-          file,
-          id,
-        },
-        dispatch
-      );
-    } catch (error) {
-      throw Error(error);
-    }
-  };
-
-  return (
-    <div className={`scroll_y_black_white ${styles.container}`}>
-      <div className={styles.containerSm}>
-        <div className={styles.backgroundContainer}>
-          <h4>Welcome back to</h4>
-          <h2>Innovate Socials</h2>
-        </div>
-
-        <div className={styles.formDiv}>
-          <div className="flex align_center column">
-            <h3>CREATE ACCOUNT</h3>
-            <h5>Create your own personal account</h5>
-          </div>
-
-          <form onChange={(e) => handleInput(e)} onSubmit={onSub}>
-            <input name="user_name" placeholder="Username..." />
-            <input name="name" placeholder="Name..." />
-            <input name="email" placeholder="Email..." />
-            <input name="password" type="password" placeholder="Password..." />
-            <label
-              className=" flex align_center gap05rem"
-              htmlFor="imageSelector"
-            >
-              <Image src={imagePicker} alt="select image" />
-              <p className="font12">Select profile picture</p>
-            </label>
-            <input
-              hidden
-              style={{ padding: "initial" }}
-              type="file"
-              name="image"
-              onChange={(e) => setFile(e.target.files[0])}
-              id="imageSelector"
-            />
-            <div className="flex column gap05rem">
-              <button>Register</button>
-              {detailsError &&
-                setTimeout(() => {
-                  dispatch(loginFailure(false));
-                }, 2000) && (
-                  <span className="red font14 fontW600 textCenter">
-                    {detailsError}
-                  </span>
-                )}
-            </div>
-
-            <p>
-              Have an account?{" "}
-              <a href="/login" className="blue text_decoration_none">
-                Login
-              </a>
-            </p>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+const page = async () => {
+  const session = await getServerSession();
+  if (session) {
+    redirect("/");
+  }
+  return <Form />;
 };
 
-export default Registration;
+export default page;
--- app/utils/api_requests/auth.js
+++ app/utils/api_requests/auth.js
@@ -12,25 +12,33 @@ import { signIn } from "next-auth/react";
 export const registerAuthentication = async ({ data, file, id }, dispatch) => {
   const currentTime = Timestamp.now().seconds;
   const storageRef = ref(storage, `${data?.email}/profile/${id}`);
-  await uploadBytesResumable(storageRef, file);
-  const image = await getDownloadURL(storageRef);
 
-  await setDoc(doc(db, "users", id), {
-    displayName: data.name,
-    uid: id,
-    email: data.email,
-    userImage: image,
-    time: currentTime,
-  });
-  const user = { ...data, work: id };
-  if (image) {
-    user.image = image;
-  }
-  const reg = await signIn("signup", { ...user, redirect: false });
-  if (reg.error) {
-    dispatch(loginFailure(reg.error));
-    const desertRef = ref(storage, storageRef);
-    await deleteObject(desertRef);
-    await deleteDoc(doc(db, "users", id));
+  if (storageRef) {
+    await uploadBytesResumable(storageRef, file);
+    const image = await getDownloadURL(storageRef);
+    if (image) {
+      await setDoc(doc(db, "users", id), {
+        displayName: data.name,
+        uid: id,
+        email: data.email,
+        userImage: image,
+        time: currentTime,
+      });
+      const user = { ...data, work: id };
+      if (image) {
+        user.image = image;
+      }
+      const reg = await signIn("signup", { ...user, redirect: false });
+      if (reg.error) {
+        dispatch(loginFailure(reg.error));
+        const desertRef = ref(storage, storageRef);
+        await deleteObject(desertRef);
+        await deleteDoc(doc(db, "users", id));
+      }
+    } else {
+      dispatch(loginFailure("Something went wrong"));
+    }
+  } else {
+    dispatch(loginFailure("Something went wrong"));
   }
 };
