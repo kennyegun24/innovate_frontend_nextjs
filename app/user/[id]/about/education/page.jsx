a77f8c416626bbaf33d5203020bc2ddd21c4af61
Kenny Elias <kennyegun240@gmail.com>
2023-10-29 23:51:51 +0100

    Files moved to new path..

--- app/user/[id]/about/education/page.jsx
+++
@@ -1,9 +0,0 @@
-import ProfileEducation from "@/app/components/profile/profileAbout/ProfileEducation";
-import React from "react";
-import { otherUserDetails } from "@/app/_mock/user_details";
-
-const CurrentUserEducation = ({ params }) => {
-  return <ProfileEducation data={otherUserDetails[params.id].education} />;
-};
-
-export default CurrentUserEducation;
--- app/user/[id]/about/experience/page.jsx
+++
@@ -1,9 +0,0 @@
-import ProfileExperience from "@/app/components/profile/profileAbout/ProfileExperience";
-import React from "react";
-
-import { otherUserDetails } from "@/app/_mock/user_details";
-const CurrentUserExperience = ({ params }) => {
-  return <ProfileExperience data={otherUserDetails[params.id].experience} />;
-};
-
-export default CurrentUserExperience;
