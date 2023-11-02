b5e8ff5a161c16f5de3619f1b3ee26bf73868a17
Kenny Elias <kennyegun240@gmail.com>
2023-11-02 21:04:01 +0100

    Modify styling Change rolling loading animation

--- app/components/animaate/RollingAnimation.jsx
+++ app/components/animaate/RollingAnimation.jsx
@@ -3,8 +3,7 @@ import React from "react";
 const RollingAnimation = () => {
   return (
     <div className="rollDiv">
-      <h5>Loading...</h5>
-      <p className="roll" />
+      <div class="custom-loader" />
     </div>
   );
 };
