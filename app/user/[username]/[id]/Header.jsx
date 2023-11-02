76a50a9c7c397eceacaf11dd2102df39099b3d59
Kenny Elias <kennyegun240@gmail.com>
2023-11-02 16:14:35 +0100

    Modify and fetch user details in header component

--- app/user/[username]/[id]/Header.jsx
+++ app/user/[username]/[id]/Header.jsx
@@ -2,21 +2,45 @@
 import React from "react";
 import ProfileHeader from "@/app/components/profile/profileHeader/ProfileHeader";
 import { usePathname } from "next/navigation";
-import { useSelector } from "react-redux";
 import ProfileHeaderAnimation from "@/app/components/animaate/ProfileHeaderAnimation";
+import useSWR from "swr";
+import { useDispatch } from "react-redux";
+import { getDetails } from "@/app/redux/unauth/otherUserDetalisSlice";
 
 const Header = ({ id }) => {
-  const { details, loading } = useSelector((state) => state.unauthUserDetails);
+  const dispatch = useDispatch();
   const path = usePathname();
   const userName = path.split("/")[2];
   const editProfile = `/user/${userName}/${id}`;
   const feeds = `/user/${userName}/${id}`;
   const about = `/user/${userName}/${id}/about`;
   const friends = `/user/${userName}/${id}/friends`;
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    `http://localhost:4000/api/v1/unauth/user_details/${id}`,
+    fetcher,
+    {
+      refreshInterval: null,
+      // errorRetryInterval: null,
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+      errorRetryCount: 0,
+      revalidateOnMount: 10000,
+    }
+  );
+  const details = data?.data || {};
+  if (data) {
+    dispatch(getDetails(data.data));
+  }
+
   return (
     <div>
-      {loading ? (
-        <ProfileHeaderAnimation />
+      {isLoading ? (
+        <>
+          <ProfileHeaderAnimation />
+        </>
       ) : (
         <ProfileHeader
           feeds={feeds}
