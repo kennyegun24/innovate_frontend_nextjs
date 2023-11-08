e555baf05f56fd5d68dfd06948766629ae570e5a
Kenny Elias <kennyegun240@gmail.com>
2023-11-08 16:10:07 +0100

    Fetch chat from firestore in real time

--- app/components/chat/chatContentComps/ChatBody.jsx
+++ app/components/chat/chatContentComps/ChatBody.jsx
@@ -1,38 +1,71 @@
-import React from "react";
+"use client";
+import React, { memo, useEffect, useRef, useState } from "react";
 import styles from "./chatBody.module.css";
 import { currentUserDetails } from "@/app/_mock/current_user_details";
 import { _mock_chat_message } from "@/app/_mock/chat_content";
+import { useSelector } from "react-redux";
+import { useSession } from "next-auth/react";
+import { db } from "@/app/firebase";
+import { onSnapshot, doc } from "firebase/firestore";
 
 const ChatBody = () => {
-  const { id } = currentUserDetails;
+  const { data } = useSession();
+  const { details } = useSelector((state) => state.unauthUserDetails);
+  const smoothSlide = useRef();
+  const currentUserUid = data?.user?.uid;
+  const combineId =
+    data?.user?.uid && currentUserUid > details?.uid
+      ? data?.user?.uid + details?.uid
+      : details?.uid + data?.user?.uid;
+  const id = currentUserUid;
+
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    if (combineId && data?.user?.uid) {
+      const unsub = onSnapshot(doc(db, "chats", combineId), (doc) => {
+        console.log(doc.data());
+        setMessages(doc.data()?.messages || []);
+      });
+      // unsub();
+      return () => {
+        unsub();
+      };
+    }
+  }, [combineId, data?.user?.uid, details?.uid]);
+
+  useEffect(() => {
+    smoothSlide.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <div
-      className="scroll_y padding05rem flex column gap1rem"
+      className="scroll_y_black_white padding05rem flex column gap1rem"
       style={{
         maxHeight: "100%",
       }}
     >
-      {_mock_chat_message.map((chat, index) => (
+      {messages.map((chat, index) => (
         <div
           key={index}
           className="white-space flex width100"
           style={{
-            flexDirection: chat.sender_id === id ? "row-reverse" : "row",
+            flexDirection: chat.senderId === id ? "row-reverse" : "row",
           }}
+          ref={smoothSlide}
         >
           <div
             className="background theme width_fit padding05rem "
             style={{
               borderRadius:
-                chat.sender_id === id ? "6px 6px 0 6px" : "6px 6px 6px 0",
+                chat.senderId === id ? "6px 6px 0 6px" : "6px 6px 6px 0",
               maxWidth: "80%",
             }}
           >
-            <p className=" font12">{chat.message}</p>
+            <p className=" font12">{chat.text}</p>
             <p
               className="font10 opacity05"
               style={{
-                textAlign: chat.sender_id === id ? "right" : "left",
+                textAlign: chat.senderId === id ? "right" : "left",
                 paddingTop: "0.5rem",
               }}
             >
@@ -45,4 +78,4 @@ const ChatBody = () => {
   );
 };
 
-export default ChatBody;
+export default memo(ChatBody);
