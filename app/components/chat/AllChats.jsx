b6e54816632b000b2f0ceb53c170df01b9038794
Kenny Elias <kennyegun240@gmail.com>
2023-11-08 16:09:17 +0100

    Query user chats from firebase

--- app/components/chat/AllChats.jsx
+++ app/components/chat/AllChats.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { _mock_all_chats } from "@/app/_mock/all_message";
 import Image from "next/image";
 import Link from "next/link";
 import Search from "antd/es/input/Search";
+import { db } from "@/app/firebase";
+import { doc, onSnapshot } from "firebase/firestore";
+import { useSession } from "next-auth/react";
 
 const AllChats = () => {
+  const { data } = useSession();
+  const [users, setUsers] = useState({});
+
+  useEffect(() => {
+    if (data?.user?.uid) {
+      const getChats = () => {
+        const unsub = onSnapshot(
+          doc(db, "userchat", data?.user?.uid),
+          (doc) => {
+            setUsers(doc.data() || {});
+          }
+        );
+        return () => {
+          unsub();
+        };
+      };
+      data?.user?.uid && getChats();
+
+      return () => {
+        getChats(); // Unsubscribe from the snapshot listener when the component unmounts
+      };
+    }
+  }, [data?.user?.uid]);
   return (
     <div className="flex column gap05rem ">
       <div
@@ -17,37 +44,47 @@ const AllChats = () => {
       >
         <Search placeholder="Search users..." />
       </div>
-      {_mock_all_chats.map((user, index) => (
-        <div className="padding05rem flex column gap05rem" key={index}>
-          <Link
-            href={`/chat/${user.sender_id}/${user.chat_id}`}
-            className="flex gap05rem text_decoration_none text_color"
-          >
-            <Image
-              src={user.image}
-              alt=""
-              width={50}
-              height={50}
-              className="roundedImage object-cover"
-            />
-            <div
-              className="flex column gap02rem"
-              style={{
-                flex: 1,
-              }}
-            >
-              <p className="font14">{user.display_name}</p>
-              <p className="font12">
-                {user.last_message.length >= 60
-                  ? user.last_message.slice(0, 60) + "..."
-                  : user.last_message}
-              </p>
-              <p className="font10 opacity05">{user.time}</p>
+      {Object.entries(users)
+        .sort((a, b) => b[1]?.date - a[1]?.date)
+        .map((user, index) => {
+          return (
+            <div className="padding05rem flex column gap05rem" key={index}>
+              <Link
+                href={`/chat/${user[1]?.userInfo.displayName}/${user[1]?.userInfo.uid}`}
+                className="flex gap05rem text_decoration_none text_color"
+              >
+                <Image
+                  src={user[1]?.userInfo?.photoURL}
+                  alt=""
+                  width={40}
+                  height={40}
+                  className="roundedImage object-cover"
+                />
+                <div
+                  className="flex column gap02rem"
+                  style={{
+                    flex: 1,
+                  }}
+                >
+                  <p className="font12">
+                    {user[1]?.userInfo?.displayName?.length >= 12
+                      ? user[1]?.userInfo?.displayName?.slice(0, 12) + "..."
+                      : user[1]?.userInfo?.displayName}
+                  </p>
+                  <p className="font10">
+                    {user[1]?.lastMessage?.text?.length >= 25
+                      ? user[1]?.lastMessage?.text?.slice(0, 25) + "..."
+                      : user[1]?.lastMessage?.text}
+                  </p>
+                  <p className="font10 opacity05">
+                    {new Date(user[1].date?.seconds * 1000).toLocaleString()}
+                  </p>
+                </div>
+              </Link>
+              <hr className="hr" />
             </div>
-          </Link>
-          <hr className="hr" />
-        </div>
-      ))}
+          );
+        })}
     </div>
   );
 };
